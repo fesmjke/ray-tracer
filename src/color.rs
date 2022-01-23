07b4c2a89eab4dd933d118f370c3f7d65e38b1e6
fesmjke <fatalerror.sm@gmail.com>
2022-01-23 17:58:19 +0200

    changed type Vec3 to Color

--- src/color.rs
+++ src/color.rs
@@ -1,8 +1,8 @@
 use std::io::{Stdout, Write};
 
-use crate::vec3::vec3::Vec3;
+use crate::vec3::vec3::{Color};
 
-pub fn write_color(out : &mut Stdout,pixel_color : Vec3) {
+pub fn write_color(out : &mut Stdout,pixel_color : Color) {
 
     let buf = format!("{} {} {}\n",pixel_color.r(),pixel_color.g(),pixel_color.b());
 
