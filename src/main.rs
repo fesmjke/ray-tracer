75e47b77d4b41b93f06604cdc9b618f2b65d5baf
fesmjke <fatalerror.sm@gmail.com>
2022-01-23 17:44:55 +0200

    changed output of color using std out stream

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self,Write};
 
 mod vec3;
+mod color;
 
 fn main() {
     let image_width = 256;
@@ -8,9 +9,11 @@ fn main() {
 
     println!("P3\n {} {}\n255\n",image_width,image_height);
 
+    let mut out = io::stdout();
+
     for j in (0..image_height).rev(){
         let indicator = format!("\rScan lines remaining: {} ",j);
-        io::stderr().write(indicator.as_bytes()); io::stdout().flush();
+        io::stderr().write(indicator.as_bytes()); out.flush();
         for i in 0..image_width {
             let r = i as f32 / image_width as f32;
             let g = j as f32 / image_height as f32;
@@ -20,7 +23,9 @@ fn main() {
             let ig = (255.999 * g) as i32;
             let ib = (255.999 * b) as i32;
 
-            println!("{} {} {}",ir,ig,ib);
+            let pixel_color = vec3::vec3::Vec3::new(ir as f32,ig as f32,ib as f32);
+
+            color::write_color(&mut out,pixel_color);
         }
         io::stderr().write(b"\nDone.\n");
     }
