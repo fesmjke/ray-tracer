3ee8b40eaa55cd3a98392348e13f95bc2df8028a
fesmjke <fatalerror.sm@gmail.com>
2022-01-27 19:13:26 +0200

    added camera settings,and first ray trace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io::{self,Write};
+use crate::color::ray_color;
+use crate::ray::ray::Ray;
+use crate::vec3::vec3::{Color, Point3, Vec3};
 
 mod vec3;
+mod ray;
 mod color;
 
 fn main() {
@@ -8,7 +12,18 @@ fn main() {
     // Image settings
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-    let image_height = (image_width/aspect_ratio) as i32;
+    let image_height = (image_width as f32 /aspect_ratio) as i32;
+
+    // Camera settings
+
+    let viewport_height = 2.0 as f32;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0f32,0f32,0f32);
+    let horizontal = Vec3::new(viewport_width,0f32,0f32);
+    let vertical = Vec3::new(0f32,viewport_height,0f32);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0f32,0f32,focal_length);
 
     println!("P3\n {} {}\n255\n",image_width,image_height);
 
@@ -18,15 +33,18 @@ fn main() {
         let indicator = format!("\rScan lines remaining: {} ",j);
         io::stderr().write(indicator.as_bytes()); out.flush();
         for i in 0..image_width {
-            let r = i as f32 / image_width as f32;
-            let g = j as f32 / image_height as f32;
-            let b = 0.25;
+            let u = i as f32 / (image_width) as f32;
+            let v = j as f32 / (image_height) as f32;
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+            let ray = Ray::ray(origin,lower_left_corner + u*horizontal + v*vertical - origin);
 
-            let pixel_color = vec3::vec3::Color::new(ir as f32,ig as f32,ib as f32);
+            let fallen_color = ray_color(&ray);
+
+            let ir = (255.999 * fallen_color.r()) as i32;
+            let ig = (255.999 * fallen_color.g()) as i32;
+            let ib = (255.999 * fallen_color.b()) as i32;
+
+            let pixel_color= Color::new(ir as f32,ig as f32,ib as f32);
 
             color::write_color(&mut out,pixel_color);
         }
