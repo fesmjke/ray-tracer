cab2cd0f65465381e8a82f64a546aa426aeb6f44
fesmjke <fatalerror.sm@gmail.com>
2023-09-28 19:14:26 +0300

    feat: removed spehere hit and changed color function

--- src/color.rs
+++ src/color.rs
@@ -1,21 +1,8 @@
 use std::io::{Stdout, Write};
+use crate::hit::{Hit, Hittable, HittableList};
 
 use crate::ray::Ray;
 use crate::vec3::{Color, Point3, Vec3};
-
-pub fn hit_sphere(center: &Point3, radius: f32, ray: &Ray) -> f32 {
-    let oc: Vec3 = ray.origin() - *center;
-    let a = Vec3::dot(&ray.direction(), &ray.direction());
-    let b = 2.0 * Vec3::dot(&oc, &ray.direction());
-    let c = Vec3::dot(&oc, &oc) - (radius * radius);
-    let disc = b * b - 4.0 * a * c;
-
-    if disc < 0.0 {
-        return -1.0;
-    } else {
-        return (-b - f32::sqrt(disc))  / (2.0 * a);
-    }
-}
 
 pub fn write_color(out: &mut Stdout, pixel_color: Color) {
     let buf = format!(
@@ -28,17 +15,14 @@ pub fn write_color(out: &mut Stdout, pixel_color: Color) {
     out.write(buf.as_bytes()).expect("Unable to write color data!");
 }
 
-pub fn ray_color(ray: &Ray) -> Color {
-    let mut transition = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, ray);
-    
-    if transition > 0.0 {
-        let temp = ray.at(transition) - Vec3::new(0.0, 0.0, -1.0);
-        let n : Vec3 = Vec3::unit_vector(&temp);
-        
-        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
-    }
+pub fn world_color(ray: &Ray, world: &HittableList) -> Color {
+    let mut temp_hit = Hit::new();
 
-    let unit_direction = Vec3::unit_vector(&ray.direction());
-    transition = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - transition) * Color::new(1f32, 1f32, 1f32) + transition * Color::new(0.5f32, 0.7f32, 1.0f32)
+    return if world.hit(&ray, 0.0, f32::MAX, &mut temp_hit) {
+        0.5 * Color::new(temp_hit.normal.x() + 1.0, temp_hit.normal.y() + 1.0, temp_hit.normal.z() + 1.0)
+    } else {
+        let unit_direction = Vec3::unit_vector(&ray.direction());
+        let transition = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - transition) * Color::new(1f32, 1f32, 1f32) + transition * Color::new(0.5f32, 0.7f32, 1.0f32)
+    }
 }
