093bd93f9ea1070ef275460b206b3399ef4c00b2
fesmjke <fatalerror.sm@gmail.com>
2023-10-03 21:14:53 +0300

    feat+fix: added set hit, and fixed bug with close objects

--- src/hit.rs
+++ src/hit.rs
@@ -1,5 +1,5 @@
 use crate::ray::Ray;
-use crate::vec3::{Vec3};
+use crate::vec3::Vec3;
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool;
@@ -9,7 +9,7 @@ pub trait Hittable {
 pub struct Hit {
     pub t: f32,
     pub p: Vec3,
-    pub normal: Vec3
+    pub normal: Vec3,
 }
 
 impl Hit {
@@ -20,16 +20,22 @@ impl Hit {
             normal: Vec3::empty_new(),
         }
     }
+
+    pub fn set_hit(&mut self, hit: &Hit) {
+        self.p = hit.p;
+        self.normal = hit.normal;
+        self.t = hit.t;
+    }
 }
 
 pub struct HittableList {
-    hittable_list: Vec<Box<dyn Hittable>>
+    hittable_list: Vec<Box<dyn Hittable>>,
 }
 
 impl HittableList {
     pub fn new() -> Self {
         Self {
-            hittable_list : Vec::new()
+            hittable_list: Vec::new(),
         }
     }
 
@@ -42,13 +48,13 @@ impl Hittable for HittableList {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool {
         let mut temp_hit = Hit::new();
         let mut hit_any = false;
-        let mut closest = t_max; // replace to f64?
+        let mut closest = t_max;
 
         for object in self.hittable_list.iter() {
-            if object.hit(&ray, t_min, t_max, &mut temp_hit) {
+            if object.hit(&ray, t_min, closest, &mut temp_hit) {
                 hit_any = true;
                 closest = temp_hit.t;
-                let _ = std::mem::replace(hit, temp_hit); // look at that : "hit = temp_hit";
+                hit.set_hit(&temp_hit);
             }
         }
 
