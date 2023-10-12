da6122aa725ed9a6269d403352070a43cfd71e9d
fesmjke <fatalerror.sm@gmail.com>
2023-10-12 18:46:28 +0300

    feat: added materials module, and material trait

---
+++ src/materials/material.rs
@@ -0,0 +1,7 @@
+use crate::hit::Hit;
+use crate::ray::Ray;
+use crate::vec3::Color;
+
+pub trait Material {
+    fn scatter(&self, ray_in: &Ray, hit: &Hit, attenuation: &Color, ray_scattered: &mut Ray);
+}
