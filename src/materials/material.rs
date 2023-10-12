408e9ea28517a23f36a971f5c465edb4919ab62d
fesmjke <fatalerror.sm@gmail.com>
2023-10-12 19:02:25 +0300

    fix: changed color to mut and added return type

--- src/materials/material.rs
+++ src/materials/material.rs
@@ -3,5 +3,11 @@ use crate::ray::Ray;
 use crate::vec3::Color;
 
 pub trait Material {
-    fn scatter(&self, ray_in: &Ray, hit: &Hit, attenuation: &Color, ray_scattered: &mut Ray);
+    fn scatter(
+        &self,
+        ray_in: &Ray,
+        hit: &Hit,
+        attenuation: &mut Color,
+        ray_scattered: &mut Ray,
+    ) -> bool;
 }
