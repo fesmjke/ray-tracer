d7ecda491d50074396dc8b09c501c0c86a06341d
fesmjke <fatalerror.sm@gmail.com>
2023-10-16 11:42:12 +0300

    feat: added preset for diffrent computations (fast, complex)

---
+++ src/preset.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::env::Args;
+
+pub enum Preset {
+    Fast { samples_per_pixel: i32, depth: u32 },
+    Complex { samples_per_pixel: i32, depth: u32 },
+}
+
+pub fn parse_preset(args: Args) -> Option<Preset> {
+    let mut preset = None;
+
+    for arg in args.into_iter() {
+        match arg.as_str() {
+            "-fast" => {
+                preset = Some(Preset::Fast {
+                    samples_per_pixel: 10,
+                    depth: 10,
+                })
+            }
+            "-complex" => {
+                preset = Some(Preset::Complex {
+                    samples_per_pixel: 100,
+                    depth: 50,
+                })
+            }
+            _ => {
+                preset = Some(Preset::Fast {
+                    samples_per_pixel: 10,
+                    depth: 10,
+                })
+            }
+        };
+    }
+
+    preset
+}
