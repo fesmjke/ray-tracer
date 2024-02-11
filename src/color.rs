f2923a806903d558e9e9072fdb85113e945b4274
fesmjke <fatalerror.sm@gmail.com>
2024-02-11 22:57:50 +0200

    test: added cover tests

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,7 @@
 use crate::float_eq::{ApproxEq, EPSILON};
 use std::ops::{Add, Div, Mul, Sub};
 
+#[derive(Copy, Clone, Debug)]
 pub struct Color {
     r: f64,
     g: f64,
@@ -107,5 +108,89 @@ impl Div<f64> for Color {
             g: self.g / other,
             b: self.b / other,
         }
+    }
+}
+
+#[cfg(test)]
+mod color_tests {
+    use super::*;
+
+    #[test]
+    fn color_creation() {
+        let df_color = Color::default();
+        let custom_color = Color::new(0.9, 0.6, 0.75);
+
+        assert_eq!(df_color.g, 0.0);
+        assert_eq!(custom_color.b, 0.75);
+    }
+
+    #[test]
+    fn color_addition() {
+        let color_a = Color::new(0.1, 0.4, 0.2);
+        let color_b = Color::new(0.1, 0.1, 0.2);
+
+        let color_addition = color_a + color_b;
+        assert_eq!(color_addition, Color::new(0.2, 0.5, 0.4));
+
+        let color_addition = color_b + color_a;
+        assert_eq!(color_addition, Color::new(0.2, 0.5, 0.4));
+
+        let color_addition = color_a + color_a;
+        assert_eq!(color_addition, Color::new(0.2, 0.8, 0.4));
+    }
+
+    #[test]
+    fn color_subtraction() {
+        let color_a = Color::new(0.1, 0.4, 0.2);
+        let color_b = Color::new(0.1, 0.1, 0.2);
+
+        let expected_color = Color::new(0.0, 0.3, 0.0);
+        let color_subtraction = color_a - color_b;
+        assert_eq!(expected_color, color_subtraction);
+
+        let color_subtraction = color_b - color_a;
+        let expected_color = Color::new(0.0, -0.3, 0.0);
+        assert_eq!(expected_color, color_subtraction);
+
+        let color_subtraction = color_a - color_a;
+        let expected_color = Color::new(0.0, 0.0, 0.0);
+        assert_eq!(expected_color, color_subtraction);
+    }
+
+    #[test]
+    fn color_multiplication() {
+        let color_a = Color::new(0.3, 0.0, 1.0);
+        let color_b = Color::new(0.2, 0.0, 1.0);
+
+        let expected_color = Color::new(0.6, 0.0, 1.0);
+        let color_multiplication = color_a * color_b;
+        assert_eq!(expected_color, color_multiplication);
+    }
+
+    #[test]
+    fn color_multiplication_scalar() {
+        let color_a = Color::new(0.35, 0.0, 0.45);
+
+        let expected_color = Color::new(0.7, 0.0, 0.9);
+        let color_multiplication = color_a * 2.0;
+        assert_eq!(expected_color, color_multiplication);
+    }
+
+    #[test]
+    fn color_division() {
+        let color_a = Color::new(0.1, 0.1, 0.1);
+        let color_b = Color::new(0.1, 0.1, 0.1);
+
+        let expected_color = Color::new(1.0, 1.0, 1.0);
+        let color_division = color_a / color_b;
+        assert_eq!(expected_color, color_division);
+    }
+    #[test]
+    fn color_division_scalar() {
+        let color_a = Color::new(1.0, 1.0, 1.0);
+
+        let expected_color = Color::new(0.5, 0.5, 0.5);
+        let color_division = color_a / 2.0;
+        assert_eq!(expected_color, color_division);
     }
 }
