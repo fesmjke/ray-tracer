7e9b3650dbbccdc28c239edb104bc72cce95844c
fesmjke <fatalerror.sm@gmail.com>
2024-02-11 23:43:16 +0200

    feat: moved from binary to library type of project

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod camera;
+pub mod canvas;
+pub mod color;
+pub mod float_eq;
+pub mod hit;
+pub mod material;
+pub mod preset;
+pub mod ray;
+pub mod utils;
+pub mod vec3;
+pub mod vector;
+pub mod objects {
+    pub mod plane;
+    pub mod sphere;
+    pub mod triangle;
+}
