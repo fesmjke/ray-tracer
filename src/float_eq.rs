86c05f418f34d5525a0b9f4ea785edac12a9f13b
fesmjke <fatalerror.sm@gmail.com>
2024-02-18 03:15:57 +0200

    feat: applied ApproxEq to f64 type

--- src/float_eq.rs
+++ src/float_eq.rs
@@ -3,3 +3,9 @@ pub const EPSILON: f64 = 1.0e-7;
 pub trait ApproxEq<Rhs = Self> {
     fn approx_eq(&self, other: &Rhs) -> bool;
 }
+
+impl ApproxEq for f64 {
+    fn approx_eq(&self, other: &Self) -> bool {
+        (self - other).abs() < EPSILON
+    }
+}
