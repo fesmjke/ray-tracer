f759d330f2f7ec8f5909c41a91d24d290de2c018
fesmjke <fatalerror.sm@gmail.com>
2024-02-18 03:24:30 +0200

    feat: added matrices to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod camera;
 pub mod canvas;
 pub mod color;
+mod entity;
 pub mod float_eq;
 pub mod hit;
 pub mod material;
@@ -15,4 +16,15 @@ pub mod objects {
     pub mod plane;
     pub mod sphere;
     pub mod triangle;
+}
+
+pub mod matrices {
+    pub use matrix::Matrix;
+    pub use matrix2::Matrix2;
+    pub use matrix3::Matrix3;
+    pub use matrix4::Matrix4;
+    mod matrix;
+    mod matrix2;
+    mod matrix3;
+    mod matrix4;
 }
