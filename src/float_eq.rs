d09fab491de86b4031e222830ea6ec0f1fa0b418
fesmjke <fatalerror.sm@gmail.com>
2024-02-29 16:35:48 +0200

    feat: added approx eq for low epsilon

--- src/float_eq.rs
+++ src/float_eq.rs
@@ -1,11 +1,19 @@
 pub const EPSILON: f64 = 1.0e-7;
+pub const LOW_EPSILON: f64 = 1.0e-4;
 
 pub trait ApproxEq<Rhs = Self> {
     fn approx_eq(&self, other: &Rhs) -> bool;
+    fn approx_eq_low(&self, other: &Rhs) -> bool {
+        self.approx_eq(other)
+    }
 }
 
 impl ApproxEq for f64 {
     fn approx_eq(&self, other: &Self) -> bool {
         (self - other).abs() < EPSILON
+    }
+
+    fn approx_eq_low(&self, other: &Self) -> bool {
+        (self - other).abs() < LOW_EPSILON
     }
 }
