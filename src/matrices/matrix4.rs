d2a4ed534437a11cf6ff707d693bdf3e99f33ade
fesmjke <fatalerror.sm@gmail.com>
2024-03-01 19:01:29 +0200

    feat+fix: added multiplication with point, fixed multiplication with vector

--- src/matrices/matrix4.rs
+++ src/matrices/matrix4.rs
@@ -1,5 +1,6 @@
 use crate::float_eq::ApproxEq;
 use crate::matrices::{Matrix, Matrix3};
+use crate::point::Point;
 use crate::vector::Vector3;
 use std::ops::Mul;
 
@@ -82,6 +83,19 @@ impl Mul<Vector3> for Matrix4 {
     type Output = Vector3;
 
     fn mul(self, other: Vector3) -> Self::Output {
+        // horrible solution
+        Self::Output::new(
+            self.data[0][0] * other.x + self.data[0][1] * other.y + self.data[0][2] * other.z,
+            self.data[1][0] * other.x + self.data[1][1] * other.y + self.data[1][2] * other.z,
+            self.data[2][0] * other.x + self.data[2][1] * other.y + self.data[2][2] * other.z,
+        )
+    }
+}
+
+impl Mul<Point> for Matrix4 {
+    type Output = Point;
+
+    fn mul(self, other: Point) -> Self::Output {
         // horrible solution
         Self::Output::new(
             self.data[0][0] * other.x
