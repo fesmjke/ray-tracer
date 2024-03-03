fb22d27c80be29b7c37bec6f297b3c5018cceda3
fesmjke <fatalerror.sm@gmail.com>
2024-03-03 20:49:46 +0200

    fix: added public access

--- src/transformations/builder.rs
+++ src/transformations/builder.rs
@@ -11,26 +11,26 @@ impl<T> TransformBuilder<T>
 where
     T: Transformable,
 {
-    fn transform(self) -> T {
+    pub fn transform(self) -> T {
         self.inner.transform(&self.transformation)
     }
 
-    fn translate(mut self, x: f64, y: f64, z: f64) -> Self {
+    pub fn translate(mut self, x: f64, y: f64, z: f64) -> Self {
         self.transformation = Translate(x, y, z).transformation() * self.transformation;
         self
     }
 
-    fn rotate(mut self, over: Over, angle: f64) -> Self {
+    pub fn rotate(mut self, over: Over, angle: f64) -> Self {
         self.transformation = Rotate(over, angle).transformation() * self.transformation;
         self
     }
 
-    fn scale(mut self, x: f64, y: f64, z: f64) -> Self {
+    pub fn scale(mut self, x: f64, y: f64, z: f64) -> Self {
         self.transformation = Scale(x, y, z).transformation() * self.transformation;
         self
     }
 
-    fn shear(mut self, xy: f64, xz: f64, yx: f64, yz: f64, zx: f64, zy: f64) -> Self {
+    pub fn shear(mut self, xy: f64, xz: f64, yx: f64, yz: f64, zx: f64, zy: f64) -> Self {
         self.transformation = Shear(xy, xz, yx, yz, zx, zy).transformation() * self.transformation;
         self
     }
