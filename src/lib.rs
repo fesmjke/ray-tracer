9d5ca923cdf30dd90f62eafdfa174a4b8f94978e
fesmjke <fatalerror.sm@gmail.com>
2024-03-07 19:10:52 +0200

    feat: updated lib mod usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,14 @@
-pub mod camera;
 pub mod canvas;
 pub mod color;
-mod entity;
 pub mod float_eq;
-pub mod hit;
-pub mod material;
 pub mod point;
 pub mod preset;
 pub mod ray;
 pub mod utils;
-pub mod vec3;
 pub mod vector;
 
-pub mod objects {
-    pub mod plane;
-    pub mod sphere;
-    pub mod triangle;
+pub mod primitives {
+    mod sphere;
 }
 
 pub mod matrices {
