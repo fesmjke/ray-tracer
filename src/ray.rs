0ec690d85e080eed0ee05f255dde14350b5a1e10
fesmjke <fatalerror.sm@gmail.com>
2024-03-07 19:53:16 +0200

    tests: added ray intersection sphere

--- src/ray.rs
+++ src/ray.rs
@@ -29,6 +29,7 @@ impl Default for Ray {
 #[cfg(test)]
 mod ray_tests {
     use super::*;
+    use crate::primitives::Sphere;
 
     #[test]
     fn ray_creation() {
@@ -57,5 +58,45 @@ mod ray_tests {
         assert_eq!(Point::new(3.0, 3.0, 4.0), ray.position(1.0));
         assert_eq!(Point::new(1.0, 3.0, 4.0), ray.position(-1.0));
         assert_eq!(Point::new(4.5, 3.0, 4.0), ray.position(2.5));
+    }
+
+    #[test]
+    fn ray_intersect_sphere_at_two_points() {
+        let ray = Ray::new(Point::new(0.0, 0.0, -5.0), Vector3::new(0.0, 0.0, 1.0));
+        let sphere = Sphere::default();
+        let intersects = sphere.intersect(&ray);
+        let expected_intersects = vec![4.0, 6.0];
+
+        assert_eq!(expected_intersects, intersects);
+    }
+
+    #[test]
+    fn ray_intersect_sphere_at_one_point() {
+        let ray = Ray::new(Point::new(0.0, 1.0, -5.0), Vector3::new(0.0, 0.0, 1.0));
+        let sphere = Sphere::default();
+        let intersects = sphere.intersect(&ray);
+        let expected_intersects = vec![5.0, 5.0];
+
+        assert_eq!(expected_intersects, intersects);
+    }
+
+    #[test]
+    fn ray_originated_inside_intersects_sphere() {
+        let ray = Ray::new(Point::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.0, 1.0));
+        let sphere = Sphere::default();
+        let intersects = sphere.intersect(&ray);
+        let expected_intersects = vec![-1.0, 1.0];
+
+        assert_eq!(expected_intersects, intersects);
+    }
+
+    #[test]
+    fn ray_behind_sphere() {
+        let ray = Ray::new(Point::new(0.0, 0.0, 5.0), Vector3::new(0.0, 0.0, 1.0));
+        let sphere = Sphere::default();
+        let intersects = sphere.intersect(&ray);
+        let expected_intersects = vec![-6.0, -4.0];
+
+        assert_eq!(expected_intersects, intersects);
     }
 }
