1dabcd9f0b0df024e49a5f707d1d861374810929
fesmjke <fatalerror.sm@gmail.com>
2024-03-10 15:57:35 +0200

    feat+test: added color reflection of material, and tests

--- src/material.rs
+++ src/material.rs
@@ -1,4 +1,7 @@
 use crate::color::Color;
+use crate::lights::PointLight;
+use crate::point::Point;
+use crate::vector::Vector3;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Material {
@@ -21,6 +24,40 @@ impl Material {
             specular,
             shininess,
         }
+    }
+
+    pub fn color_reflection(
+        &self,
+        light: PointLight,
+        position: Point,
+        eye_vector: Vector3,
+        normal_vector: Vector3,
+    ) -> Color {
+        let effective_color = self.color * light.intensity;
+        let delta = (light.position - position).normalize();
+        let light_vector = Vector3::new(delta.x, delta.y, delta.z);
+        let ambient = effective_color * self.ambient;
+        let mut diffuse = Color::default();
+        let mut specular = Color::default();
+
+        let light_dot = light_vector.dot(&normal_vector);
+
+        if light_dot < 0.0 {
+            diffuse = Color::black();
+            specular = Color::black();
+        } else {
+            diffuse = effective_color * self.diffuse * light_dot;
+            let reflect_vector = -light_vector.reflect(&normal_vector);
+            let reflect_dot_eye = reflect_vector.dot(&eye_vector);
+            if reflect_dot_eye <= 0.0 {
+                specular = Color::black();
+            } else {
+                let factor = reflect_dot_eye.powf(self.shininess);
+                specular = light.intensity * self.specular * factor;
+            }
+        }
+
+        return ambient + diffuse + specular;
     }
 
     pub fn color(mut self, color: Color) -> Self {
@@ -64,7 +101,10 @@ impl Default for Material {
 #[cfg(test)]
 mod material_tests {
     use crate::color::Color;
+    use crate::lights::PointLight;
     use crate::material::Material;
+    use crate::point::Point;
+    use crate::vector::Vector3;
 
     #[test]
     fn material_default_creation() {
@@ -80,5 +120,85 @@ mod material_tests {
         assert_eq!(expected_diffuse, default_material.diffuse);
         assert_eq!(expected_specular, default_material.specular);
         assert_eq!(expected_shininess, default_material.shininess);
+    }
+
+    #[test]
+    fn material_reflects_color_between_light_and_surface() {
+        let material = Material::default();
+        let position = Point::default();
+        let eye_vector = Vector3::new(0.0, 0.0, -1.0);
+        let normal_vector = Vector3::new(0.0, 0.0, -1.0);
+        let light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::new(0.0, 0.0, -10.0));
+
+        let expected_color = Color::new(1.9, 1.9, 1.9);
+
+        assert_eq!(
+            expected_color,
+            material.color_reflection(light, position, eye_vector, normal_vector)
+        );
+    }
+
+    #[test]
+    fn material_reflects_color_between_light_and_surface_eye_offset_45_degree() {
+        let material = Material::default();
+        let position = Point::default();
+        let eye_vector = Vector3::new(0.0, f64::sqrt(2.0) / 2.0, -f64::sqrt(2.0) / 2.0);
+        let normal_vector = Vector3::new(0.0, 0.0, -1.0);
+        let light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::new(0.0, 0.0, -10.0));
+
+        let expected_color = Color::new(1.0, 1.0, 1.0);
+
+        assert_eq!(
+            expected_color,
+            material.color_reflection(light, position, eye_vector, normal_vector)
+        );
+    }
+
+    #[test]
+    fn material_reflects_color_between_light_and_surface_light_offset_45_degree() {
+        let material = Material::default();
+        let position = Point::default();
+        let eye_vector = Vector3::new(0.0, 0.0, -1.0);
+        let normal_vector = Vector3::new(0.0, 0.0, -1.0);
+        let light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::new(0.0, 10.0, -10.0));
+
+        let expected_color = Color::new(0.7364, 0.7364, 0.7364);
+
+        assert_eq!(
+            expected_color,
+            material.color_reflection(light, position, eye_vector, normal_vector)
+        );
+    }
+
+    #[test]
+    fn material_reflects_color_between_light_and_surface_reflection() {
+        let material = Material::default();
+        let position = Point::default();
+        let eye_vector = Vector3::new(0.0, -f64::sqrt(2.0) / 2.0, -f64::sqrt(2.0) / 2.0);
+        let normal_vector = Vector3::new(0.0, 0.0, -1.0);
+        let light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::new(0.0, 10.0, -10.0));
+
+        let expected_color = Color::new(1.6364, 1.6364, 1.6364);
+
+        assert_eq!(
+            expected_color,
+            material.color_reflection(light, position, eye_vector, normal_vector)
+        );
+    }
+
+    #[test]
+    fn material_reflects_color_behind_surface() {
+        let material = Material::default();
+        let position = Point::default();
+        let eye_vector = Vector3::new(0.0, 0.0, -1.0);
+        let normal_vector = Vector3::new(0.0, 0.0, -1.0);
+        let light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::new(0.0, 0.0, 10.0));
+
+        let expected_color = Color::new(0.1, 0.1, 0.1);
+
+        assert_eq!(
+            expected_color,
+            material.color_reflection(light, position, eye_vector, normal_vector)
+        );
     }
 }
