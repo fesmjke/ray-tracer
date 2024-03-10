13864400d0b3218682b6b26427de94307fad22b5
fesmjke <fatalerror.sm@gmail.com>
2024-03-10 14:13:55 +0200

    feat: added point lights sources

---
+++ src/lights/point_light.rs
@@ -0,0 +1,41 @@
+use crate::color::Color;
+use crate::point::Point;
+
+pub struct PointLight {
+    intensity: Color,
+    position: Point,
+}
+
+impl PointLight {
+    pub fn new(intensity: Color, position: Point) -> Self {
+        Self {
+            intensity,
+            position,
+        }
+    }
+}
+
+impl Default for PointLight {
+    fn default() -> Self {
+        Self {
+            intensity: Color::default(),
+            position: Point::default(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod point_light_tests {
+    use crate::color::Color;
+    use crate::lights::point_light::PointLight;
+    use crate::point::Point;
+
+    #[test]
+    fn point_light_creation() {
+        let point_light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::default());
+        let expected_intensity = Color::new(1.0, 1.0, 1.0);
+
+        assert_eq!(expected_intensity, point_light.intensity);
+        assert_eq!(Point::default(), point_light.position);
+    }
+}
