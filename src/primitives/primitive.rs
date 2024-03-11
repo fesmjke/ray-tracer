ed83f8c3d7041b27c48e6f6c9b796b5511f5ebf6
fesmjke <fatalerror.sm@gmail.com>
2024-03-11 14:53:20 +0200

    feat: added dynamic equality for objects

--- src/primitives/primitive.rs
+++ src/primitives/primitive.rs
@@ -2,8 +2,23 @@ use crate::intersections::Intersections;
 use crate::point::Point;
 use crate::ray::Ray;
 use crate::vector::Vector3;
+use std::any::Any;
+use std::fmt::Debug;
 
-pub trait Primitive {
+pub trait Primitive: Debug + Any {
     fn intersect(&self, ray: &Ray) -> Intersections;
     fn normal(&self, world: Point) -> Vector3;
+
+    fn as_any(&self) -> &dyn Any;
+
+    fn equals_to(&self, other: &dyn Primitive) -> bool
+    where
+        Self: Sized + PartialEq + Any,
+    {
+        if let Some(other) = other.as_any().downcast_ref::<Self>() {
+            *self == *other
+        } else {
+            false
+        }
+    }
 }
