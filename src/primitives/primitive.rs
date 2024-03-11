1a30c825ffe8d8f7bbe63051df364dd189ad80ba
fesmjke <fatalerror.sm@gmail.com>
2024-03-11 17:42:59 +0200

    feat: extended primitive trait, added primitve shape enum

--- src/primitives/primitive.rs
+++ src/primitives/primitive.rs
@@ -1,24 +1,47 @@
 use crate::intersections::Intersections;
+use crate::material::Material;
 use crate::point::Point;
+use crate::primitives::PrimitiveShape::SphereShape;
+use crate::primitives::Sphere;
 use crate::ray::Ray;
 use crate::vector::Vector3;
-use std::any::Any;
-use std::fmt::Debug;
 
-pub trait Primitive: Debug + Any {
+pub trait Primitive {
     fn intersect(&self, ray: &Ray) -> Intersections;
     fn normal(&self, world: Point) -> Vector3;
+    fn material(&self) -> Material;
+}
 
-    fn as_any(&self) -> &dyn Any;
+#[derive(Debug, Clone)]
+pub enum PrimitiveShape {
+    SphereShape(Sphere),
+}
 
-    fn equals_to(&self, other: &dyn Primitive) -> bool
-    where
-        Self: Sized + PartialEq + Any,
-    {
-        if let Some(other) = other.as_any().downcast_ref::<Self>() {
-            *self == *other
-        } else {
-            false
+impl Primitive for PrimitiveShape {
+    fn intersect(&self, ray: &Ray) -> Intersections {
+        match self {
+            SphereShape(sphere) => sphere.intersect(ray),
+        }
+    }
+
+    fn normal(&self, world: Point) -> Vector3 {
+        match self {
+            SphereShape(sphere) => sphere.normal(world),
+        }
+    }
+
+    fn material(&self) -> Material {
+        match self {
+            SphereShape(sphere) => sphere.material,
+        }
+    }
+}
+
+impl PartialEq for PrimitiveShape {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (SphereShape(sphere_a), SphereShape(sphere_b)) => sphere_a == sphere_b,
+            // _ => panic!("DIFFERENT TYPE OF SHAPES"),
         }
     }
 }
