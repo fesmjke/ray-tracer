1c554f7bc2b95c1e44ac7aa6884b57115db87fad
fesmjke <fatalerror.sm@gmail.com>
2024-03-13 15:43:10 +0200

    feat+test: added inside field, and tests

--- src/intersections/intersection_details.rs
+++ src/intersections/intersection_details.rs
@@ -11,13 +11,21 @@ pub struct IntersectionDetails {
     pub point: Point,
     pub normal_vector: Vector3,
     pub eye_vector: Vector3,
+    pub inside: bool,
 }
 
 impl IntersectionDetails {
     pub fn from(intersection: Intersection, ray: &Ray) -> Self {
         let point = ray.position(intersection.time);
         let eye_vector = -ray.direction;
-        let normal_vector = intersection.object.normal(point);
+        let mut normal_vector = intersection.object.normal(point);
+
+        let inside = if normal_vector.dot(&eye_vector) < 0.0 {
+            normal_vector = -normal_vector;
+            true
+        } else {
+            false
+        };
 
         Self {
             time: intersection.time,
@@ -25,8 +33,20 @@ impl IntersectionDetails {
             object: intersection.object,
             normal_vector,
             eye_vector,
+            inside,
         }
     }
+
+    // fn is_inside(mut self) -> Self {
+    //     if self.normal_vector.dot(&self.eye_vector) < 0.0 {
+    //         self.normal_vector = -self.normal_vector;
+    //         self.inside = true;
+    //     } else {
+    //         self.inside = false;
+    //     }
+    //
+    //     self
+    // }
 }
 
 #[cfg(test)]
@@ -49,6 +69,27 @@ mod intersection_details_tests {
             object: sphere.clone(),
             eye_vector: Vector3::new(0.0, 0.0, -1.0),
             normal_vector: Vector3::new(0.0, 0.0, -1.0),
+            inside: false,
+        };
+
+        assert_eq!(
+            expected_details,
+            IntersectionDetails::from(intersection, &ray)
+        );
+    }
+
+    #[test]
+    fn intersection_details_occurs_on_the_inside() {
+        let ray = Ray::new(Point::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.0, 1.0));
+        let sphere = SphereShape(Sphere::default());
+        let intersection = Intersection::new(1.0, sphere.clone());
+        let expected_details = IntersectionDetails {
+            time: 1.0,
+            point: Point::new(0.0, 0.0, 1.0),
+            object: sphere.clone(),
+            eye_vector: Vector3::new(0.0, 0.0, -1.0),
+            normal_vector: Vector3::new(0.0, 0.0, -1.0),
+            inside: true,
         };
 
         assert_eq!(
