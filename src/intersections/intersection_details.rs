ded15c09f0fcbdac2471a5d5a9dfde63112380d0
fesmjke <fatalerror.sm@gmail.com>
2024-03-14 17:48:24 +0200

    feat+test: added overpoint field, and test

--- src/intersections/intersection_details.rs
+++ src/intersections/intersection_details.rs
@@ -1,3 +1,4 @@
+use crate::float_eq::LOW_EPSILON;
 use crate::intersections::Intersection;
 use crate::point::Point;
 use crate::primitives::{Primitive, PrimitiveShape};
@@ -9,6 +10,7 @@ pub struct IntersectionDetails {
     pub time: f64,
     pub object: PrimitiveShape,
     pub point: Point,
+    pub over_point: Point,
     pub normal_vector: Vector3,
     pub eye_vector: Vector3,
     pub inside: bool,
@@ -27,9 +29,12 @@ impl IntersectionDetails {
             false
         };
 
+        let over_point = point + normal_vector * LOW_EPSILON;
+
         Self {
             time: intersection.time,
             point,
+            over_point,
             object: intersection.object.clone(),
             normal_vector,
             eye_vector,
@@ -51,11 +56,13 @@ impl IntersectionDetails {
 
 #[cfg(test)]
 mod intersection_details_tests {
+    use crate::float_eq::LOW_EPSILON;
     use crate::intersections::{Intersection, IntersectionDetails};
     use crate::point::Point;
     use crate::primitives::PrimitiveShape::SphereShape;
     use crate::primitives::Sphere;
     use crate::ray::Ray;
+    use crate::transformations::Transformable;
     use crate::vector::Vector3;
 
     #[test]
@@ -70,6 +77,7 @@ mod intersection_details_tests {
             eye_vector: Vector3::new(0.0, 0.0, -1.0),
             normal_vector: Vector3::new(0.0, 0.0, -1.0),
             inside: false,
+            over_point: Point::new(0.0, 0.0, -1.0001),
         };
 
         assert_eq!(
@@ -90,11 +98,24 @@ mod intersection_details_tests {
             eye_vector: Vector3::new(0.0, 0.0, -1.0),
             normal_vector: Vector3::new(0.0, 0.0, -1.0),
             inside: true,
+            over_point: Point::new(0.0, 0.0, 0.9999),
         };
 
         assert_eq!(
             expected_details,
             IntersectionDetails::from(&intersection, &ray)
         );
+    }
+
+    #[test]
+    fn intersection_details_should_offset_point() {
+        let ray = Ray::new(Point::new(0.0, 0.0, -5.0), Vector3::new(0.0, 0.0, 1.0));
+        let sphere = SphereShape(Sphere::default().translate(0.0, 0.0, 1.0).transform());
+        let intersection = Intersection::new(5.0, sphere);
+        let intersection_details = IntersectionDetails::from(&intersection, &ray);
+        let expected_position_z = -LOW_EPSILON / 2.0;
+
+        assert!(expected_position_z > intersection_details.over_point.z);
+        assert!(intersection_details.point.z > intersection_details.over_point.z);
     }
 }
