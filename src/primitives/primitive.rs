b110c3f68976a72056133abaafdad159db008c5e
fesmjke <fatalerror.sm@gmail.com>
2024-03-16 16:41:01 +0200

    feat: added plane shape and plane handlers

--- src/primitives/primitive.rs
+++ src/primitives/primitive.rs
@@ -1,8 +1,8 @@
 use crate::intersections::Intersections;
 use crate::material::Material;
 use crate::point::Point;
-use crate::primitives::PrimitiveShape::SphereShape;
-use crate::primitives::Sphere;
+use crate::primitives::PrimitiveShape::{PlaneShape, SphereShape};
+use crate::primitives::{Plane, Sphere};
 use crate::ray::Ray;
 use crate::vector::Vector3;
 
@@ -15,24 +15,28 @@ pub trait Primitive {
 #[derive(Debug, Clone)]
 pub enum PrimitiveShape {
     SphereShape(Sphere),
+    PlaneShape(Plane),
 }
 
 impl Primitive for PrimitiveShape {
     fn intersect(&self, ray: &Ray) -> Intersections {
         match self {
             SphereShape(sphere) => sphere.intersect(ray),
+            PlaneShape(plane) => plane.intersect(ray),
         }
     }
 
     fn normal(&self, world: Point) -> Vector3 {
         match self {
             SphereShape(sphere) => sphere.normal(world),
+            PlaneShape(plane) => plane.normal(world),
         }
     }
 
     fn material(&self) -> Material {
         match self {
             SphereShape(sphere) => sphere.material,
+            PlaneShape(plane) => plane.material,
         }
     }
 }
@@ -41,7 +45,8 @@ impl PartialEq for PrimitiveShape {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (SphereShape(sphere_a), SphereShape(sphere_b)) => sphere_a == sphere_b,
-            // _ => panic!("DIFFERENT TYPE OF SHAPES"),
+            (PlaneShape(plane_a), PlaneShape(plane_b)) => plane_a == plane_b,
+            _ => panic!("DIFFERENT TYPE OF SHAPES"),
         }
     }
 }
