46d7f4daad6e74fca4062c227524ea9850f1d2cd
fesmjke <fatalerror.sm@gmail.com>
2024-05-19 22:59:03 +0300

    feat: added hits and normal computation

--- src/primitives/triangle.rs
+++ src/primitives/triangle.rs
@@ -1,10 +1,14 @@
 use crate::point::Point;
+use crate::vector::Vector3;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
-struct Triangle {
+pub struct Triangle {
     pub point_a: Point,
     pub point_b: Point,
     pub point_c: Point,
+    pub e_hit_a: Vector3,
+    pub e_hit_b: Vector3,
+    pub normal: Vector3,
 }
 
 impl Triangle {
@@ -13,10 +17,17 @@ impl Triangle {
     }
 
     pub fn from(pa: Point, pb: Point, pc: Point) -> Self {
+        let e1: Vector3 = (pb - pa).into();
+        let e2: Vector3 = (pc - pa).into();
+        let normal = (e2.cross(&e1)).normalize();
+
         Self {
             point_a: pa,
             point_b: pb,
             point_c: pc,
+            e_hit_a: e1,
+            e_hit_b: e2,
+            normal,
         }
     }
 }
@@ -27,6 +38,9 @@ impl Default for Triangle {
             point_a: Point::default(),
             point_b: Point::default(),
             point_c: Point::default(),
+            e_hit_a: Default::default(),
+            e_hit_b: Default::default(),
+            normal: Default::default(),
         }
     }
 }
