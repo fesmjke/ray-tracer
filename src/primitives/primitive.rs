13cbd2d69e9eff03d838ba6b26f8b93af8f210b6
fesmjke <fatalerror.sm@gmail.com>
2024-05-20 23:31:42 +0300

    feat: added triangle shape to primitives

--- src/primitives/primitive.rs
+++ src/primitives/primitive.rs
@@ -2,8 +2,8 @@ use crate::intersections::Intersections;
 use crate::material::Material;
 use crate::matrices::Matrix4;
 use crate::point::Point;
-use crate::primitives::PrimitiveShape::{CubeShape, PlaneShape, SphereShape};
-use crate::primitives::{Cube, Plane, Sphere};
+use crate::primitives::PrimitiveShape::{CubeShape, PlaneShape, SphereShape, TriangleShape};
+use crate::primitives::{Cube, Plane, Sphere, Triangle};
 use crate::ray::Ray;
 use crate::transformations::Transformable;
 use crate::vector::Vector3;
@@ -21,6 +21,7 @@ pub enum PrimitiveShape {
     SphereShape(Sphere),
     PlaneShape(Plane),
     CubeShape(Cube),
+    TriangleShape(Triangle),
 }
 
 impl Primitive for PrimitiveShape {
@@ -37,6 +38,10 @@ impl Primitive for PrimitiveShape {
             CubeShape(cube) => {
                 let ray = &ray.transform(&cube.transformation_inverse);
                 cube.intersect(ray)
+            }
+            TriangleShape(triangle) => {
+                let ray = &ray.transform(&triangle.transformation_inverse);
+                triangle.intersect(ray)
             }
         }
     }
@@ -68,6 +73,14 @@ impl Primitive for PrimitiveShape {
 
                 world_normal.normalize()
             }
+            TriangleShape(triangle) => {
+                let transformation_inverted = triangle.transformation_inverse;
+                let local_point = transformation_inverted * *world;
+                let local_normal = triangle.normal(&local_point);
+                let world_normal = triangle.transformation_inverse_transpose * local_normal;
+
+                world_normal.normalize()
+            }
         }
     }
 
@@ -76,6 +89,7 @@ impl Primitive for PrimitiveShape {
             SphereShape(sphere) => sphere.material.clone(),
             PlaneShape(plane) => plane.material.clone(),
             CubeShape(cube) => cube.material.clone(),
+            TriangleShape(triangle) => triangle.material.clone(),
         }
     }
 
@@ -84,6 +98,7 @@ impl Primitive for PrimitiveShape {
             SphereShape(sphere) => sphere.transformation(),
             PlaneShape(plane) => plane.transformation(),
             CubeShape(cube) => cube.transformation(),
+            TriangleShape(triangle) => triangle.transformation(),
         }
     }
 
@@ -92,6 +107,7 @@ impl Primitive for PrimitiveShape {
             SphereShape(sphere) => sphere.transformation_invert(),
             PlaneShape(plane) => plane.transformation_invert(),
             CubeShape(cube) => cube.transformation_invert(),
+            TriangleShape(triangle) => triangle.transformation_invert(),
         }
     }
 }
@@ -102,6 +118,7 @@ impl PartialEq for PrimitiveShape {
             (SphereShape(sphere_a), SphereShape(sphere_b)) => sphere_a == sphere_b,
             (PlaneShape(plane_a), PlaneShape(plane_b)) => plane_a == plane_b,
             (CubeShape(cube_a), CubeShape(cube_b)) => cube_a == cube_b,
+            (TriangleShape(triangle_a), TriangleShape(triangle_b)) => triangle_a == triangle_b,
             _ => false,
         }
     }
